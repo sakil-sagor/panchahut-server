c7661b3ad85a05944b31c10dbd0968a74e5fc419
sakil-sagor <sakilsagor76543@gmail.com>
2024-05-20 10:03:42 +0600

    make find category

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -1,9 +1,27 @@
-const { createCategoryInDb } = require("../services/category.service");
+const {
+  createCategoryInDb,
+  findSingleCategoryInDb,
+  deleteCategoryInDb,
+} = require("../services/category.service");
 
 // create report
 exports.createCategory = async (req, res) => {
   try {
     const result = await createCategoryInDb(req.body);
+    res.status(200).json({
+      status: "success",
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: "Couldn't get the Products",
+    });
+  }
+};
+exports.singleCategory = async (req, res) => {
+  try {
+    const result = await findSingleCategoryInDb();
     res.status(200).json({
       status: "success",
       data: result,
--- services/category.service.js
+++ services/category.service.js
@@ -4,6 +4,10 @@ exports.createCategoryInDb = async (details) => {
   const result = await Category.create(details);
   return result;
 };
+exports.findSingleCategoryInDb = async () => {
+  const result = await Category.create();
+  return result;
+};
 exports.deleteCategoryInDb = async () => {
   const result = await Category.create();
   return result;
